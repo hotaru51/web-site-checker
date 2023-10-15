7fe461e4f0b59f76c4da1d975492b0309027479c
hotaru <36354847+hotaru51@users.noreply.github.com>
2023-10-16 00:06:00 +0900

    WebページとXPathの文字列取得までを実装

--- web_site_checker/app.rb
+++ web_site_checker/app.rb
@@ -1,11 +1,32 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'logger'
+
+require_relative 'lib/history_data'
+require_relative 'lib/clawler'
+require_relative 'lib/dynamodb_history_table'
 
 module WebSiteChecker
+  # Lambda handler
   class Handler
+    HISTORY_TABLE_NAME = ENV['HISTORY_TABLE_NAME']
+
     def self.process(event:, context:)
-      {statusCode: 200, body: 'Web site checker function'}.to_json
+      logger = Logger.new($stdout)
+      logger.info("event: #{JSON.generate(event)}")
+
+      url = event['url']
+      xpath = event['xpath']
+      subject = event['subject']
+
+      # WebページとXPathの文字列取得
+      logger.info("fetch web page: url=#{url}, xpath=#{xpath}")
+      cl = WebSiteChecker::Clawler.new(url)
+      text = cl.search(xpath)
+      logger.info("clawler result: #{text}")
+
+      {statusCode: 200, body: 'done'}.to_json
     end
   end
 end
